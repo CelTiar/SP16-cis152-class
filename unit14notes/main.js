86c29a90110b3c0ec03d7b028fc699660979f79a
Eric Barnard <eric.m.barnard@gmail.com>
2016-04-07 12:42:30 -0500

    notes from class on 4/7

---
+++ unit14notes/main.js
@@ -0,0 +1,40 @@
+
+console.log("Hello World!");
+
+console.log(1);
+console.log(1.02);
+
+console.log(2 + 2);
+
+console.log(2 == 2);
+console.log(2 != 2);
+
+console.log("two plus two equals: " + (2 + 2));
+
+console.log(2 + (3 * (4 / 2)));
+
+// declare variables
+var x = 0;
+var y = 0;
+
+// set variables 
+x = 225;
+y = 356;
+
+// perform math equations using the variables
+var answer = x + y;
+console.log(answer);
+
+// typeof tells us what data type a variable is
+console.log("The typeof x is: " + typeof(x));
+console.log("The typeof y is: " + typeof(y));
+
+
+console.log('Eric said "Quotes are awesome!"');
+console.log('Eric said "Quotes are awesome!" and \'Single quotes are awesome too\'');
+
+
+alert("Oh NO - you just formatted your hard drive!!");
+
+var response = prompt("Hey, what's your name?");
+alert("Hey " + response + ", are you free this weekend?");
